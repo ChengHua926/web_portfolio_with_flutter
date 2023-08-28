3775a059e796587f92c55900030c04f21c30a62a
KanyeEast0 <huacheng615@gmail.com>
2023-08-28 16:32:52 +0800

    trying to change app strings

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,6 +1,6 @@
 abstract class AppStrings {
-  static const String developerName = 'Rady Haggag';
-  static const String helloIM = 'Hello, i’m';
+  static const String developerName = 'Cheng Hua';
+  static const String helloIM = 'Hello, I’m';
   static const String introMsg =
       "Flutter developer with +2 years of experience. I have completed many projects that prove my ability to provide creative and high-quality solutions.";
   static String aboutMeMsg =
